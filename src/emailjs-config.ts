6ca24b3dca13389b8f0f8a2edc743e73803296ab
ZelmaSedano <zvsedano@gmail.com>
2026-06-27 07:48:00 -0500

    removing emailjs-config.ts from .gitignore to fix vercel deployment

---
+++ src/emailjs-config.ts
@@ -0,0 +1,6 @@
+// src/emailjs-config.ts
+export const EMAILJS_CONFIG = {
+    SERVICE_ID: 'service_gwprl37',
+    TEMPLATE_ID: 'template_nneudiv', // Replace with your actual template ID
+    PUBLIC_KEY: 'LjhiQbPlXFn9EYsCD' // Replace with your actual public key
+};
